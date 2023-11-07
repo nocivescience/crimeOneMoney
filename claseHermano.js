40d30c2b48ce7b2e5b893d3a86367a143f14dd6d
nocive.science <nocive.science@gmail.com>
2023-11-07 01:03:22 -0300

    primer commit al master

---
+++ claseHermano.js
@@ -0,0 +1,35 @@
+class Hermano{
+    constructor(imagenes, x, y, animatedPause=false){
+        this.imagenes = imagenes;
+        this.x = x;
+        this.y = y;
+        this.animatedPause = animatedPause;
+        this.currentFigure=imagenes[0];
+        this.imagenes.forEach((element, i) => {
+            element.style.position = "absolute";
+            element.style.left = `${this.x}px`;
+            element.style.top = `${this.y}px`;
+            // element.style.width = "90%";
+            // element.style.height = "auto";
+            // element.style.transform = 'translateX(8%)';
+            element.style.zIndex = "1";
+            // element.style.userSelect = "none";
+            document.querySelector('body').appendChild(element);
+            element.style.display = "none";
+        });
+    }
+    minMaxRandom(min, max){
+        return Math.floor(Math.random() * (max - min)) + min;
+    }
+    alternateFigures(){
+        if(!this.animatedPause){
+            this.currentFigure.style.display='none';
+            this.currentFigure = this.imagenes[(this.imagenes.indexOf(this.currentFigure)+1)%this.imagenes.length];
+        }
+        this.currentFigure.style.display='block';
+        setTimeout(()=>{this.alternateFigures()}, 80);
+    }
+    pause(){
+        this.animatedPause = true;
+    }
+}
